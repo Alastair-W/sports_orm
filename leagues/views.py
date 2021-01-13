420db2d32c55b835dae0425a71fe8e0d057d3fa1
RowdyOnline <rowdy14@gmail.com>
2021-01-13 09:51:03 -0800

    Sports ORM II complete

--- leagues/views.py
+++ leagues/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
+from django.db.models import Count
 
 from . import team_maker
 
@@ -12,7 +13,7 @@ def index(request):
 		"nonFootballLeagues": League.objects.exclude(sport="Football"),
 		"conferences": League.objects.filter(name__contains="Conference"),
 		"atlantic": League.objects.filter(name__contains="Atlantic"),
-		"teams": Team.objects.all(),
+		"teams": Team.objects.all().order_by("location", "team_name"),
 		"dallasTeams": Team.objects.filter(location="Dallas"),
 		"raptorTeams": Team.objects.filter(team_name="Raptors"),
 		"cityTeams": Team.objects.filter(location__contains="City"),
@@ -23,7 +24,21 @@ def index(request):
 		"cooperPlayers": Player.objects.filter(last_name="Cooper"),
 		"joshuaPlayers": Player.objects.filter(first_name="Joshua"),
 		"cooperNotJoshua": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
-		"alexanderOrWyatt": Player.objects.filter(first_name="Alexander") | Player.objects.filter(first_name="Wyatt")
+		"alexanderOrWyatt": Player.objects.filter(first_name="Alexander") | Player.objects.filter(first_name="Wyatt"),
+		"atlanticHockey": League.objects.filter(name__contains="Atlantic", sport__contains="Hockey"),
+		"teamBosSta": Team.objects.filter(location="Boston", team_name="Stallions"),
+		"intBaseConf": Team.objects.filter(league=League.objects.get(name="International Baseball Conference")),
+		"amFedSoc": Team.objects.filter(league=League.objects.get(name="Atlantic League of Ice Hockey")),
+		"footballLeagues": League.objects.filter(sport="Football"),
+		"teamsSophia": Player.objects.filter(first_name="Sophia"),
+		"teamsGonzales": Player.objects.filter(last_name="Gonzales"),
+		"johnHarris": Player.objects.get(first_name="John", last_name="Harris"),
+		"ontarioColts": Team.objects.get(location="Ontario", team_name="Colts"),
+		"nevadaBJ": Team.objects.get(location="Nevada", team_name="Blue Jays"),
+		"jacksonWhite": Player.objects.get(first_name="Jackson", last_name="White"),
+		"teamPlayerCount": Team.objects.annotate(player_count=Count('all_players')).order_by('-player_count'),
+		"allPlayers": Player.objects.annotate(numTeams=Count('all_teams')).order_by('-numTeams')
+
 	}
 	return render(request, "leagues/index.html", context)
 
